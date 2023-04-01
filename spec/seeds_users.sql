f603aa5da9bb4cd54c98c13c44aa71cb1212ef0e
Jack <jwleggett@outlook.com>
2023-04-01 11:16:50 +0100

    created database tables, seeded users and created user classes

---
+++ spec/seeds_users.sql
@@ -0,0 +1,7 @@
+TRUNCATE TABLE users RESTART IDENTITY; -- replace with your own table name.
+
+-- Below this line there should only be `INSERT` statements.
+-- Replace these statements with your own seed data.
+
+INSERT INTO users (email_address, username) VALUES ('h@b.com', 'h123');
+INSERT INTO users (email_address, username) VALUES ('x@y.com', 'x000');
